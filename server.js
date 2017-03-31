9c8d23454456fda313b90494587014c83eaa72ef
Ryan Daley <rpdaley@gmail.com>
2017-03-31 12:47:51 -0400

    Clean-up

--- server.js
+++ server.js
@@ -34,12 +34,12 @@ app.use(knexLogger(knex));
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/styles", sass({
-  src: __dirname + "/styles",
-  dest: __dirname + "/public/styles",
-  debug: true,
-  outputStyle: 'expanded'
-}));
+// app.use("/styles", sass({
+//   src: __dirname + "/styles",
+//   dest: __dirname + "/public/styles",
+//   debug: true,
+//   outputStyle: 'expanded'
+// }));
 app.use(express.static("public"));
 
 // Mount all resource routes
