78601678da72866c142a35b9b552c7f2280831dd
Boris Xiao <borishaw@gmail.com>
2017-04-03 02:56:06 +0000

    try again

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
+require('dotenv').config();
+
 "use strict";
 
 let user = {};
 let all_maps = {};
-
-require('dotenv').config();
 
 const PORT = process.env.PORT || 8080;
 const ENV = process.env.ENV || "development";
